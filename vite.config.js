91202527a3691d535d196f014f157f2fc9f67367
Lucas Barbosa <lucasbarbosa_182@hotmail.com>
2025-04-06 17:57:42 -0300

    bugs

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // Garante que os caminhos sejam relativos
   plugins: [react()],
-  base: './'
-})
+});
